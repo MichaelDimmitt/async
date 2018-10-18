eca9c1e878b96d7e033b48ec658a3e43a602340f
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-10-18 19:38:19 -0400

    Fetch returns a resolved or rejected promise.
    The promise is chained and the resulting promise is returned.

--- demo4-node-fetch-example.js
+++ demo4-node-fetch-example.js
@@ -11,16 +11,12 @@ getNextMeetup().then(results => {
  * This is the second method used for retrieving the next meetup. It used a Promise.
  */
 function getNextMeetup() {
-    return new Promise((resolve, reject) => {
-        fetch('https://api.meetup.com/2/events?&sign=true&group_id=10250862&page=20&key=' + process.env.meetupapi_key)
-            .then(response => {
-                return response.json();
-            }).then(json => {
-                const meetingArray = json.results;
-                setTimeToNewYork(meetingArray);
-                resolve(meetingArray[0]);
-            }).catch(err => {
-                reject(err);
-            });
-    });
+    return fetch('https://api.meetup.com/2/events?&sign=true&group_id=10250862&page=20&key=' + process.env.meetupapi_key)
+        .then(response => {
+            return response.json();
+        }).then(json => {
+            const meetingArray = json.results;
+            setTimeToNewYork(meetingArray);
+            resolve(meetingArray[0]);
+        });
 }
