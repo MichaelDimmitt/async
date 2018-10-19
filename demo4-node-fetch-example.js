6314cdf6d180069a741cedbefd3dd25762ab3e27
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-10-18 23:12:13 -0400

    Verbose example added to explain what is occouring in javascript.

--- demo4-node-fetch-example.js
+++ demo4-node-fetch-example.js
@@ -14,6 +14,31 @@ const getNextMeetup = () =>
       return meetingArray[0];
     })
 
+/*
+ * This implementation uses a function definition and does not return by default.
+ * This implementation shows everything that goes on under the hood.
+ * A promise that succeeds is in the resolve state, when a failure occours it switches to the reject state.
+function verboseGetNextMeetup() {
+  return fetch('https://api.meetup.com/2/events?&sign=true&group_id=10250862&page=20&key=' + process.env.meetupapi_key)
+  .then(
+    response => { return response.json();},
+    err => { throw err }
+  )
+  .then(
+    json => {
+      const meetingArray = json.results;
+      setTimeToNewYork(meetingArray);
+      return meetingArray[0]; },
+    err => { throw err }
+  );
+}
+ * '.then' accepts two functions as parameters, resolve fn and reject fn
+ * commonly '.then' is used with one parameter, only to handle resolve,
+ * and using .catch to handle reject. However, '.catch' can swallow errors
+ * that could occour from a dispatched action, setState triggering render function, etc.
+ * see demo7-promise-react-example.js
+*/
+
 getNextMeetup().then(
   results => { console.log(results.name); },
   err => { console.error('Your specific error message', err); }
